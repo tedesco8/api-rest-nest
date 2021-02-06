3d9493aca66106a3c1b864e6c942207085c54e63
Pablo Tedesco <tedesco8@hotmail.com>
2021-02-06 00:59:11 -0300

    creando modulo de configuracion

---
+++ src/config/config.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { ConfigService } from './config.service';
+
+@Module({
+    providers: [
+        {
+            provide: ConfigService,
+            useValue: new ConfigService(),
+        },
+    ],
+})
+export class ConfigModule { }
